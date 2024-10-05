2a1466c5971247815f340626a4289f4c850eff1b
Jul009 <lolalde@alumnos.uaq.mx>
2024-10-05 10:41:32 -0600

    Avances

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hola Mundo")
+	router := gin.Default()
+	fmt.Println("Running App")
+	//Tomar archivos de la carpeta templates
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", gin.H{})
+	})
+	router.Run(":8000")
 }
